4ac491831270e7594798fd0e9ee6ed135f5bb859
Kavindu <156315964+KaviDJ02@users.noreply.github.com>
2025-02-17 01:28:23 +0530

    add theme

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable >
+        <Text onPress={() => console.log("Pressed")}>Delete</Text>
+      </Pressable>
+      <View style={styles.itemContainer} >
+        <Text style={{
+            fontSize: 18,
+          fontWeight: "200",
+            color: "black",
+        }}>Coffee</Text>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -13,8 +22,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#f4e78a',
     justifyContent: 'center',
   },
+  itemContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: "red",
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+  }
 });
