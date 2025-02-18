c1c1dfe6e77ab46185b9d1b65dc95a2b84b37694
Kavindu <156315964+KaviDJ02@users.noreply.github.com>
2025-02-18 16:33:39 +0530

    add storageUtill

--- app/index.tsx
+++ app/index.tsx
@@ -1,30 +1,71 @@
-import { StyleSheet, TextInput, FlatList, View, Text } from "react-native";
+import { StyleSheet, TextInput, FlatList, View, Text, LayoutAnimation } from "react-native";
 import { theme } from "../theme";
 import { ShoppingListItem } from "../components/ShoppingListItem";
-import { useState } from "react";
+import {useEffect, useState} from "react";
+import {getFromStorage, saveToStorage} from "../utils/storage";
+
+const storageKey = "shopping-list";
 
 type ShoppingListItemType = {
     id: string;
     name: string;
+    completedAtTimestamp?: number;
+    lastUpdatedTimestamp: number;
 };
 
 export default function App() {
     const [shoppingList, setShoppingList] = useState<ShoppingListItemType[]>([]);
     const [value, setValue] = useState<string>();
 
+    useEffect(() => {
+        const fetchInitial = async () => {
+            const data = await getFromStorage(storageKey);
+            if (data) {
+                LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+                setShoppingList(data);
+            }
+        };
+        fetchInitial();
+    }, []);
+
     const handleSubmit = () => {
         if (value) {
             const newShoppingList = [
-                { id: new Date().toISOString(), name: value },
+                { id: new Date().toISOString(),
+                    name: value,
+                    lastUpdatedTimestamp: Date.now() },
                 ...shoppingList,
             ];
             setShoppingList(newShoppingList);
+            saveToStorage(storageKey, newShoppingList);
             setValue(undefined);
         }
     };
 
+    const handelDelete = (id: string) => {
+        const newShoppingList = shoppingList.filter((item) => item.id !== id);
+        saveToStorage(storageKey, newShoppingList);
+        setShoppingList(newShoppingList);
+    }
+
+    const handleToggleComplete = (id: string) => {
+        const newShoppingList = shoppingList.map((item) => {
+            if (item.id === id) {
+                return {
+                    ...item,
+                    lastUpdatedTimestamp: Date.now(),
+                    completedAtTimestamp: item.completedAtTimestamp ? undefined : Date.now(),
+                };
+            }
+            return item;
+        });
+        setShoppingList(newShoppingList);
+        saveToStorage(storageKey, newShoppingList);
+    }
+
     return (
         <FlatList
+            data={orderShoppingList(shoppingList)}
             ListHeaderComponent={
                 <TextInput
                     value={value}
@@ -40,13 +81,40 @@ export default function App() {
                     <Text>Your shopping list is empty</Text>
                 </View>
             }
-            data={shoppingList}
             style={styles.container}
             contentContainerStyle={styles.contentContainer}
             stickyHeaderIndices={[0]}
-            renderItem={({ item }) => <ShoppingListItem name={item.name} />}
+            renderItem={({ item }) =>
+                <ShoppingListItem
+                    name={item.name}
+                    onDelete={() => handelDelete(item.id)}
+                    onToggleComplete={() => handleToggleComplete(item.id)}
+                    isCompleted={Boolean(item.completedAtTimestamp)}
+                />}
         ></FlatList>
     );
+}
+
+function orderShoppingList(shoppingList: ShoppingListItemType[]) {
+    return shoppingList.sort((item1, item2) => {
+        if (item1.completedAtTimestamp && item2.completedAtTimestamp) {
+            return item2.completedAtTimestamp - item1.completedAtTimestamp;
+        }
+
+        if (item1.completedAtTimestamp && !item2.completedAtTimestamp) {
+            return 1;
+        }
+
+        if (!item1.completedAtTimestamp && item2.completedAtTimestamp) {
+            return -1;
+        }
+
+        if (!item1.completedAtTimestamp && !item2.completedAtTimestamp) {
+            return item2.lastUpdatedTimestamp - item1.lastUpdatedTimestamp;
+        }
+
+        return 0;
+    });
 }
 
 const styles = StyleSheet.create({
--- components/ShoppingListItem.tsx
+++ components/ShoppingListItem.tsx
@@ -1,14 +1,17 @@
-import {Alert, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {Alert, Pressable, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {theme} from "../theme";
 import AntDesign from '@expo/vector-icons/AntDesign';
+import Feather from '@expo/vector-icons/Feather';
 
 type Props = {
     // ? use this to make the prop optional
     name: string;
     isCompleted?: boolean;
+    onDelete: () => void;
+    onToggleComplete: () => void;
 };
 
-export function ShoppingListItem({name, isCompleted}: Props) {
+export function ShoppingListItem({name, isCompleted, onDelete, onToggleComplete}: Props) {
     const handleDelete = () => {
         Alert.alert("Delete", `Are you sure you want to delete ${name} item?`, [
             {
@@ -18,20 +21,29 @@ export function ShoppingListItem({name, isCompleted}: Props) {
             },
             {
                 text: "Yes",
-                onPress: () => console.log("Yes"),
+                onPress: () => onDelete(),
                 style: "destructive",
             },
         ]);
     };
 
-    return <View style={[styles.itemContainer, isCompleted ? styles.completedContainer : undefined]} >
-        <Text style={[styles.text, isCompleted ? styles.completedText : undefined]}>{name}</Text>
+    return <Pressable
+        style={[
+            styles.itemContainer,
+            isCompleted ? styles.completedContainer : undefined
+        ]}
+        onPress={onToggleComplete}
+    >
+        <View style={styles.row}>
+            <Feather name={isCompleted? "check" : "circle"} size={24} color={isCompleted? theme.colorGray : theme.colorCerulean} />
+            <Text style={[styles.text, isCompleted ? styles.completedText : undefined]}>{name}</Text>
+        </View>
         <TouchableOpacity
             onPress={!isCompleted ? () => handleDelete(): undefined}
             activeOpacity={0.8} >
             <AntDesign name="closecircle" size={24} color={isCompleted ? theme.colorGray : theme.colorRed} />
         </TouchableOpacity>
-    </View>;
+    </Pressable>;
 }
 
 const styles = StyleSheet.create({
@@ -48,6 +60,7 @@ const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: "200",
         color: "black",
+        flex: 1,
     },
     completedContainer: {
         backgroundColor: theme.colorLightGray,
@@ -58,6 +71,12 @@ const styles = StyleSheet.create({
         textDecorationLine: "line-through",
         textDecorationColor: theme.colorGray,
         color: theme.colorGray,
+    },
+    row: {
+        flexDirection: "row",
+        gap: 4,
+        alignItems: "center",
+        flex: 1,
     }
 
 });
