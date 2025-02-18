8ed1104c141eb8c6f4a37fcd9a2a562d451a86ef
Kavindu <156315964+KaviDJ02@users.noreply.github.com>
2025-02-18 18:19:45 +0530

    add permission req

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,13 +1,23 @@
 import {Text, View, StyleSheet, TouchableOpacity} from "react-native";
 import {useRouter} from "expo-router";
+import {theme} from "../../theme";
+import {registerForPushNotificationsAsync} from "../../utils/registerForPushNotificationsAsync";
 
 export default function CounterScreen() {
-    const route = useRouter();
+    // const route = useRouter();
+
+    const handleRequestPermission = async () => {
+        const result = await registerForPushNotificationsAsync()
+        console.log(result);
+    }
 
     return (
         <View style={styles.container}>
-            <TouchableOpacity onPress={() => route.navigate("/idea")}>
-                <Text style={{ textAlign:"center", marginBottom:18, fontSize:24 }}>Go to /idea</Text>
+            {/*<TouchableOpacity onPress={() => route.navigate("/idea")}>*/}
+            {/*    <Text style={{ textAlign:"center", marginBottom:18, fontSize:24 }}>Go to /idea</Text>*/}
+            {/*</TouchableOpacity>*/}
+            <TouchableOpacity style={styles.button} activeOpacity={0.8} onPress={handleRequestPermission}>
+                <Text style={styles.buttonText}>Request Permission</Text>
             </TouchableOpacity>
             <Text style={styles.text}>Counter</Text>
         </View>
@@ -24,4 +34,15 @@ const styles = StyleSheet.create({
     text: {
         fontSize: 24,
     },
+    button: {
+        backgroundColor: theme.colorBlack,
+        padding: 12,
+        borderRadius: 6,
+    },
+    buttonText: {
+        color: "#fff",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        letterSpacing: 1,
+    }
 });
